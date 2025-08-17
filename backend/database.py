c2bc54678dfcce88c9941f20f347983143c6cdb5
Siva-0317 <sivabalaji2717@outlook.com>
2025-08-17 16:31:28 +0530

    db update

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,14 @@
+# backend/database.py
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from dotenv import load_dotenv
 
 db = SQLAlchemy()
 
 def create_app():
-    load_dotenv()
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
+    db_path = os.environ.get("DATABASE_URL", f"sqlite:///{os.path.join(os.path.dirname(__file__), 'db.sqlite3')}")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     return app
